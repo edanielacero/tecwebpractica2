b3c117e8ce3404031e0a4feb6ac14454e310167c
Daniel Acero <edanielacero@gmail.com>
2022-04-05 16:40:59 -0400

    added Logic users and made get and Post

---
+++ P2-001/Logic/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+    public class User
+    {
+        public string name { get; set; }
+    }
+}
---
+++ P2-001/Logic/UsersManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class UsersManager
+    {
+        public List<User> Users { get; set;}
+        public UsersManager()
+        {
+            Users = new List<User>()
+            {
+                new User () {name = "Edwin"},
+                new User () {name = "Daniel"}
+            };
+        }
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+        public User PostUsers(User user)
+        {
+            Users.Add(user);
+            return user;
+        }
+        public User PutUsers(User user)
+        {
+            //return true;
+        }
+        public User DeleteUsers(User user)
+        {
+            //return true;
+        }
+    }
+}
